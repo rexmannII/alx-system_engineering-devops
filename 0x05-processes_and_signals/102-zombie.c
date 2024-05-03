608735ab5f7632cafdc9d04dda253fad00bdad67
rexmannII <onyegbulerexejike@gmail.com>
2024-05-03 14:35:55 -0700

    bash scripting

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * infinite_while - Creates an infinite loop
+ *
+ * Return: Always returns 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	int i;
+	pid_t pid;
+
+	for (i = 0; i < 5; i++)
+	{
+		pid = fork();
+
+		if (pid < 0)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0)
+		{
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+		}
+	}
+
+	infinite_while();
+
+	return (0);
+}
