a0db14085199079bf1773380425109715c9b39e4
rexmannII <onyegbulerexejike@gmail.com>
2024-07-14 19:58:11 -0700

    using puppet to create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+# creating a file in /tmp using puppet
+
+file {'Ejikeme':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
+
+
