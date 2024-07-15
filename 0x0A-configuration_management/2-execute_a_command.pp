1d1cfc61d0056e975b4390796014cfed0f6659fa
rexmannII <onyegbulerexejike@gmail.com>
2024-07-14 20:30:36 -0700

    pkill_killmenow

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # creating a manifest using puppet that kills a process name killmenow
 
 
-exec {'pkill -f killmenow':
+exec { 'pkill -f killmenow':
   path   => '/usr/bin:/usr/local/bin:/bin/',
 }
