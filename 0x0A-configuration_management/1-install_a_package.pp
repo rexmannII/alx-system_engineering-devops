82d757bd7b4a916fb89ace5db3a28b65cd61f086
rexmannII <onyegbulerexejike@gmail.com>
2024-07-14 20:41:48 -0700

    installing flask using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,18 +1,18 @@
 # installing Flask using puppet
 
 
-package {'python3.8':
+package { 'python3.8':
   ensure   => '3.8.10',
   provider => 'pip3',
 }
 
-package {'Flask':
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
 
-pacakge {'Werkzeug':
+pacakge { 'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
-  require  => package['Flask']
+  require  => Package['Flask']
 }
