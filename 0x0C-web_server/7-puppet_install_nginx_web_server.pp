ab405f63187a05608766261d00f49117daf59581
rexmannII <onyegbulerexejike@gmail.com>
2024-07-15 07:38:43 -0700

    puppet install_nginx

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# automating configuration with puppet
+
+package { 'nginx':
+  ensure => installed.
+}
+
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/@RexEjikeOnyegbule-iv5wm permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
