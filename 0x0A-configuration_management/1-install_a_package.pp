fe80fb2e2d54c9565581f2d19cba5e59675ca9e0
rexmannII <onyegbulerexejike@gmail.com>
2024-07-26 08:52:37 -0700

    install_a_package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,18 +1,5 @@
-# installing Flask using puppet
-
-
-package { 'python3.8':
-  ensure   => '3.8.10',
-  provider => 'pip3',
-}
-
-package { 'Flask':
+# Install flask
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-}
-
-pacakge { 'Werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3',
-  require  => Package['Flask'],
+  provider => 'pip3'
 }
