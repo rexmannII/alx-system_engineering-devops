ea7628e006025557eb8bae4ba0677e53d7be48e9
rexmannII <onyegbulerexejike@gmail.com>
2024-08-13 05:30:37 -0700

    advanced_api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Script that queries the number of subscribers on a given Reddit subreddit and returns 0 if an invalid subreddit is given
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
